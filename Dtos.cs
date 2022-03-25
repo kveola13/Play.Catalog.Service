d4a29f7be407135b5c92b602e238cd45c6bfabb7
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-25 14:29:32 +0100

    Add project files.

---
+++ Dtos.cs
@@ -0,0 +1,10 @@
+﻿namespace Play.Catalog.Service
+{
+    public record ItemDto(Guid id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+    public class Dtos
+    {
+
+    }
+}
