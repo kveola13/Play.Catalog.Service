76fc9527e01c6b1106559a3f3230754ef848fda0
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-30 23:29:45 +0200

    added controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+        };
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            return items;
+        }
+        [HttpGet("{id}")]
+        public ItemDto GetItemById(Guid id)
+        {
+            return items.SingleOrDefault(item => item.id == id);
+        }
+    }
+}
